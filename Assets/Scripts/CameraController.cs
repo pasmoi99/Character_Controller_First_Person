279cf99085e1147f6a4627b51f91de84d2cd1e8c
pasmoi99 <144921908+pasmoi99@users.noreply.github.com>
2025-02-20 14:32:43 +0100

    Vertical camera rotations working, horzontal camera rotation pending

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private float _cameraSpeed;
+    private float _cameraVerticalRotation = 0f;
+
     private CharaController Chara;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         Chara = GameManager.MainGame.MainCharacter;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         SetPosition();
         SetRotation();
@@ -26,7 +29,20 @@ public class CameraController : MonoBehaviour
 
     private void SetRotation()
     {
-        Vector3 MousePosition = Input.mousePosition;
-        transform.LookAt(MousePosition);
+        float MouseX = _cameraSpeed * Input.GetAxis("Mouse X");
+        float MouseY = _cameraSpeed * Input.GetAxis("Mouse Y");
+
+        _cameraVerticalRotation -= MouseY;
+        _cameraVerticalRotation = Mathf.Clamp(_cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * _cameraVerticalRotation;
+
+        transform.Rotate(Vector3.up * MouseX);
+
+        //Vector3 MousePosition = Input.mousePosition;
+        //Debug.Log(MousePosition);
+        //transform.LookAt(MousePosition);
+
+        //transform.Rotate(0,0,v);
+
     }
 }
